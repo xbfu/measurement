962a7b9f08fdccbaad98a16673ed9804c241cfab
xbfu <fxb163@163.com>
2021-09-03 15:21:43 -0400

    Update prepare-dataset.sh

--- configuration/prepare-dataset.sh
+++ configuration/prepare-dataset.sh
@@ -1,5 +1,7 @@
 sudo apt install -y python3-pip
-pip3 install torchvision
+pip3 install torch==1.9.0
+pip3 install Pillow==8.2.0
+pip3 install torchvision==0.10.0
 python3 prepare_dataset.py
 sudo docker pull xbfu1994/test:v4
 
