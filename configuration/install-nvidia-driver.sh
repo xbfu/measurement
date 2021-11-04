824e17bd3c4cd0f1ce1bcb77cd5e5324905841f5
xbfu <fxb163@163.com>
2021-11-03 21:14:38 -0400

    Update install-nvidia-driver.sh

--- configuration/install-nvidia-driver.sh
+++ configuration/install-nvidia-driver.sh
@@ -4,5 +4,5 @@ sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 sudo apt-get update
-sudo apt-get -y install cuda-drivers
+sudo apt-get -y install cuda-drivers-470
 nvidia-smi
