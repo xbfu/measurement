ce40399feed7508b5621cc573c2f58db78411303
xbfu <fxb163@163.com>
2021-11-10 16:19:39 -0500

    Update install-k8s.sh

--- configuration/install-k8s.sh
+++ configuration/install-k8s.sh
@@ -2,5 +2,5 @@ sudo curl -O https://packages.cloud.google.com/apt/doc/apt-key.gpg
 sudo apt-key add apt-key.gpg
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl docker.io
+sudo apt-get install -y kubelet=1.21.1-00 kubeadm=1.21.1-00 kubectl=1.21.1-00 docker.io
 
